05a19dbabb929c847d88882d16177945e1254b81
Neel Smith <dnsmith.neel@gmail.com>
2014-11-12 14:36:15 -0500

    Add outline of concordion writing.

---
+++ markdown/writing/apsoph/ApSoph.md
@@ -0,0 +1,7 @@
+
+# Apollonius "the Sophist" #
+
+Some entries consist solely of a list of glosses or synonyms, such as the entry for δαήρ (<code
+concordion:set="#urn">urn:cts:greekLit:tlg1168.tlg001.hmt:delta.8</code>),
+where we read <strong
+concordion:assertEquals="getTokens(#urn)">ἀνδρὼς ἀδελφός</strong>.
---
+++ markdown/writing/apsoph/ApSophTest.java
@@ -0,0 +1,14 @@
+package writing.apsoph;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+/* Run this class as a JUnit test. */
+
+public class ApSophTest extends ConcordionTestCase {
+    
+    public String geTokens(String urn) {
+        return ("Never looked anything up for urn " + urn);
+    }
+}
